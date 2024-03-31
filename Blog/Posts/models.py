da289089dd25b978858b6d84815c5aee56062d1d
Ali Naeimi <130804913+alinaeimiz@users.noreply.github.com>
2024-04-01 02:26:13 +0330

    create models
    creat post and category model in database

---
+++ Blog/Posts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+# post blog model
+      
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self) -> str:
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length = 300)
+    content = models.TextField()
+    image = models.ImageField(upload_to="blogs")
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+    content_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now = True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ["published_date"]
