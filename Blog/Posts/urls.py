2e45723dcb965d11497e09a970836254835d31a2
Ali Naeimi <130804913+alinaeimiz@users.noreply.github.com>
2024-04-01 21:46:53 +0330

    add app name for apps

--- Blog/Posts/urls.py
+++ Blog/Posts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+app_name = 'post'
+
+
 urlpatterns = [
     path('', views.blog_home, name='blog-home'),
+    path('single/', views.single, name='single'),
 ]
--- Blog/Posts/views.py
+++ Blog/Posts/views.py
@@ -5,5 +5,5 @@ def blog_home(request):
     return render(request, 'blog/blog-home.html')
     
     
-def test(request):
-    return render(request, 'base.html')
+def single(request):
+    return render(request, 'blog/blog-single.html')
