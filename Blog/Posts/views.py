de7366b1fb0f7e8729f08eaa9f0593eddadc0cc2
Ali Naeimi <130804913+alinaeimiz@users.noreply.github.com>
2024-04-01 22:36:18 +0330

    set view and template of the blog-home
    in blog-home.html  set for showing post and there categories tag

--- Blog/Posts/views.py
+++ Blog/Posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def blog_home(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    return render(request, 'blog/blog-home.html',{'posts':posts})
     
     
 def single(request):
