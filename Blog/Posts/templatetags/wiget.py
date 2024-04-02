248f6866359a7b119fe42003157255b04534b76e
Ali Naeimi <130804913+alinaeimiz@users.noreply.github.com>
2024-04-02 21:52:09 +0330

    iclusion tag wiget

--- Blog/Posts/templatetags/wiget.py
+++ Blog/Posts/templatetags/wiget.py
@@ -6,3 +6,18 @@ register = template.Library()
 def popular_post():
     posts = Post.objects.filter(status = 1)[:4]
     return {'posts':posts}
+
+
+@register.inclusion_tag("wiget/post-category.html")
+def post_category():
+    posts = Post.objects.filter(status=1)
+    category = Category.objects.all()
+    counts={}
+    for name in category:
+         counts[name] = posts.filter(category=name).count()
+    return {"counts":counts}
+
+@register.inclusion_tag("wiget/cloud.html")
+def cloud():
+    category = Category.objects.all() 
+    return {'category':category}
