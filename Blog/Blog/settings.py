9aac4a2a164ebe29592a2d9c8f0d0b4803399773
Ali Naeimi <130804913+alinaeimiz@users.noreply.github.com>
2024-04-02 16:26:51 +0330

    add humize filter

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'home',
     'Posts',
+    'django.contrib.humanize',
+    
 ]
 
 MIDDLEWARE = [
